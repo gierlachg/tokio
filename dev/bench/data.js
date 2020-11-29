1a11b7e9aefd590acbb3ed14ba4895252fa52dde
github-action-benchmark <github@users.noreply.github.com>
2020-11-29 13:07:06 +0000

    add sync_mpsc (cargo) benchmark result for c55d846f4b248b4a72335d6c57829fa6396ab9a5

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1606655210889,
+  "lastUpdate": 1606655225849,
   "repoUrl": "https://github.com/gierlachg/tokio",
   "entries": {
     "rt_multi_threaded": [
@@ -47,6 +47,92 @@ window.BENCHMARK_DATA = {
             "name": "yield_many",
             "value": 18561083,
             "range": "± 3513033",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ],
+    "sync_mpsc": [
+      {
+        "commit": {
+          "author": {
+            "email": "alice@ryhl.io",
+            "name": "Alice Ryhl",
+            "username": "Darksonn"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "c55d846f4b248b4a72335d6c57829fa6396ab9a5",
+          "message": "util: add rt to tokio-util full feature (#3194)",
+          "timestamp": "2020-11-29T09:48:31+01:00",
+          "tree_id": "5f27b29cd1018796f0713d6e87e4823920ba5084",
+          "url": "https://github.com/gierlachg/tokio/commit/c55d846f4b248b4a72335d6c57829fa6396ab9a5"
+        },
+        "date": 1606655225021,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "contention_bounded",
+            "value": 6462051,
+            "range": "± 1952912",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "contention_bounded_full",
+            "value": 6485352,
+            "range": "± 1296568",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "contention_unbounded",
+            "value": 5947139,
+            "range": "± 2203596",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "create_100_000_medium",
+            "value": 772,
+            "range": "± 85",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "create_100_medium",
+            "value": 770,
+            "range": "± 113",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "create_1_medium",
+            "value": 735,
+            "range": "± 156",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "send_large",
+            "value": 51437,
+            "range": "± 1709",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "send_medium",
+            "value": 791,
+            "range": "± 122",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "uncontented_bounded",
+            "value": 1105507,
+            "range": "± 52011",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "uncontented_unbounded",
+            "value": 783613,
+            "range": "± 139842",
             "unit": "ns/iter"
           }
         ]
